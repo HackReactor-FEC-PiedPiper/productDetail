addbbad83867d0581f69323d17b5b8b9838f4da5
Utkarsh Singh <singh.ut7@gmail.com>
2020-08-12 14:33:18 -0600

    Remove hardcoded test from App.test.js This was only meant to be active prior to implementation of add-to-cart component

--- client/test/App.test.js
+++ client/test/App.test.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import App from '../src/components/App';
+import React from "react";
+import App from "../src/components/App";
 
-import { configure, shallow, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
-describe('App Parent Component', () => {
+describe("App Parent Component", () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = mount(<App />)
+    wrapper = mount(<App />);
   });
 
-
   test(`Should contain the title 'Logo'`, () => {
-    expect(wrapper.find('#nav').text()).toBe('Logo');
+    expect(wrapper.find("#nav").text()).toBe("Logo");
   });
 
   test(`Overview subcomponent should just contain text 'Overview' currently`, () => {
-    expect(wrapper.find('.overview').text()).toBe('Overview');
+    expect(wrapper.find(".overview").text()).toBe("Overview");
   });
 
   // test(`Styles subcomponent should just contain text 'Styles' currently`, () => {
   //   expect(wrapper.find('.styles').text()).to('Styles');
   // });
 
-  test(`Add to Cart subcomponent should just contain text 'Add to Cart' currently`, () => {
-    expect(wrapper.find('.a2c').text()).toBe('Add to Cart');
-  });
-
-})
+  // test(`Add to Cart subcomponent should just contain text 'Add to Cart' currently`, () => {
+  //   expect(wrapper.find('.a2c').text()).toBe('Add to Cart');
+  // });
+});
