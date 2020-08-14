b677a93787415176e4f8de8650927380e7588817
Utkarsh Singh <singh.ut7@gmail.com>
2020-08-14 15:55:30 -0600

    Implement dynamic thumbnail scrolling

--- client/src/components/Images.jsx
+++ client/src/components/Images.jsx
@@ -1,57 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Images = ({ photos, thumbnailScrollRef, scrollClick }) => (
-  <section className="carousel" aria-label="Gallery">
-    <ol className="carousel__viewport">
-      {photos.map((photo, i) => (
-        <li
-          id={`carousel__slide${i + 1}`}
-          tabIndex="0"
-          style={{ backgroundImage: `URL(${photos[i].url})` }}
-          className="carousel__slide"
-        >
-          <div className="carousel_snapper">
-            <a
-              href={i > 0 ? `#carousel__slide${i}` : `#carousel__slide${i + 1}`}
-              className="carousel__prev"
-            >
-              Go to last slide
-            </a>
-            <a href={`#carousel__slide${i + 2}`} className="carousel__next">
-              Go to next slide
-            </a>
-          </div>
-        </li>
-      ))}
-    </ol>
-    <button
-      onClick={() => scrollClick('left')}
-      id="prevThumbnails"
-      className="scrollThumbnails"
-    />
-    <aside ref={thumbnailScrollRef} className="carousel__navigation">
-      <div id="thumbnailNavigation">
-        <ol className="carousel__navigation-list">
-          {photos.map((photo, i) => (
-            <li className="carousel__navigation-item">
-              <a
-                style={{ backgroundImage: `URL(${photo.thumbnail_url})` }}
-                href={`#carousel__slide${i + 1}`}
-                className="carousel__navigation-button"
-              >
-                {`Go to slide ${i + 1}`}
-              </a>
-            </li>
-          ))}
-        </ol>
-      </div>
-    </aside>
-    <button
-      onClick={() => scrollClick('right')}
-      id="nextThumbnails"
-      className="scrollThumbnails"
-    />
-  </section>
-);
+const Images = ({ photos, thumbnailScrollRef, scrollClick }) => {
+  const [selectedSlide, setSelectedSlide] = useState(1);
+
+  const leftArrowClick = () => {
+    scrollClick('left');
+    setSelectedSlide(selectedSlide - 1);
+  };
+
+  const rightArrowClick = () => {
+    scrollClick('right');
+    setSelectedSlide(selectedSlide + 1);
+  };
+
+  return (
+    <section className="carousel" aria-label="Gallery">
+      <ol className="carousel__viewport">
+        {photos.map((photo, i) => (
+          <li
+            key={i}
+            id={`carousel__slide${i + 1}`}
+            tabIndex="0"
+            style={{ backgroundImage: `URL(${photos[i].url})` }}
+            className="carousel__slide"
+          >
+            {selectedSlide > 1 ? (
+              <div className="carousel_snapper">
+                <a
+                  onClick={leftArrowClick}
+                  href={
+                    i > 0 ? `#carousel__slide${i}` : `#carousel__slide${i + 1}`
+                  }
+                  className="carousel__prev"
+                >
+                  Go to last slide
+                </a>
+              </div>
+            ) : null}
+            {selectedSlide < photos.length ? (
+              <div className="carousel_snapper">
+                <a
+                  onClick={rightArrowClick}
+                  href={`#carousel__slide${i + 2}`}
+                  className="carousel__next"
+                >
+                  Go to next slide
+                </a>
+              </div>
+            ) : null}
+          </li>
+        ))}
+      </ol>
+      <button
+        onClick={() => scrollClick('left')}
+        id="prevThumbnails"
+        className="scrollThumbnails"
+      >
+        <i className="fas fa-arrow-left" />
+      </button>
+      <aside ref={thumbnailScrollRef} className="carousel__navigation">
+        <div id="thumbnailNavigation">
+          <ol className="carousel__navigation-list">
+            {photos.map((photo, i) => (
+              <li key={i} className="carousel__navigation-item">
+                <a
+                  onClick={() => setSelectedSlide(i + 1)}
+                  style={{ backgroundImage: `URL(${photo.thumbnail_url})` }}
+                  href={`#carousel__slide${i + 1}`}
+                  className={
+                    i + 1 === selectedSlide
+                      ? 'carousel__navigation-button-selected'
+                      : 'carousel__navigation-button'
+                  }
+                >
+                  {`Go to slide ${i + 1}`}
+                </a>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </aside>
+      <button
+        onClick={() => scrollClick('right')}
+        id="nextThumbnails"
+        className="scrollThumbnails"
+      >
+        <i className="fas fa-arrow-right" />
+      </button>
+    </section>
+  );
+};
 
 export default Images;
